50efedc00e9a85bd52f3227f1ee7a0205fd02602
Xiao_Hannah <hannaxiao0628@gmail.com>
2025-04-17 21:22:26 -0700

    homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,9 +4,9 @@ import styles from "./HomePage.module.css";
 const HomePage = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Welcome to LensAI 📸</h1>
+      <h1 className={styles.title}>Welcome to LineCraft!</h1>
       <p className={styles.subtitle}>
-        Analyze and enhance your photos with the power of AI!
+      Bring your tattoo vision to life in seconds. Our AI-powered tattoo generator turns your ideas or reference images into stunning, personalized designs. Whether you're exploring your first ink or looking for fresh inspiration, preview your custom tattoo, download it, and make it real — all in one intuitive, stylish platform.
       </p>
       <button className={styles.cta}>Get Started</button>
     </div>
